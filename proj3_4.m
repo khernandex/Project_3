442b2fb534c7744f8c02331f22479ead19a06b44
Ant0n1aM <tmarisca32@gmail.com>
2025-12-03 04:03:17 +0000

    Added 's' on the team name in order to write down the name of the team

--- proj3_4.m
+++ proj3_4.m
@@ -6,8 +6,13 @@ numPlayers = input("How many players do you want to enter? ");
 % of the player class
 for i = 1:numPlayers
     name = input("Enter the name of the baseball player: ",'s');
-    team = input("Enter the name of the team they play on: ");
+    team = input("Enter the name of the team they play on: ",'s');
     hits = input("Enter their number of hits: ");
     atBats = input("Enter their number of at bats: ");
     walks = input("Enter their number of walks: ");
+    players(i) = player(name, team, hits, walks, atBats);
 end
+
+
+
+
